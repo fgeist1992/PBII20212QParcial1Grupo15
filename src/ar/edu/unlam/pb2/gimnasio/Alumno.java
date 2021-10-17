4c5b7cdeb78f313dcd36e3d4c9da9907326f9cc8
gabtar <gabrieltarifa@gmail.com>
2021-10-16 21:02:28 -0300

    Se agregaron test de Alumno y Profesor. Se agrego .gitignore y se solucionaron algunos problemas con los .classpath de eclipse que no permitía importar bien el proyecto en eclipse

--- src/ar/edu/unlam/pb2/gimnasio/Alumno.java
+++ src/ar/edu/unlam/pb2/gimnasio/Alumno.java
@@ -6,10 +6,11 @@ public class Alumno extends Persona{
 	private Double saldo=0.0;
 	private Integer clasesInscriptas=0;
 	private Integer CANTIDAD_CLASE_BRONCE=1;
+	private Integer CANTIDAD_CLASE_PLATA=2;
 	private Double IMPORTE_ABONO_BRONCE=1000.0;
 	private Double IMPORTE_ABONO_PLATA=1800.0;
 	private Double IMPORTE_ABONO_ORO=2500.0;
-	private Integer CANTIDAD_CLASE_PLATA=2;
+	
 	public Alumno(String nombre, String apellido, Integer dni, Genero genero, Abono tipoDeAbono) {
 		super(nombre, apellido, dni, genero);
 		this.tipoDeAbono=tipoDeAbono;
@@ -43,16 +44,20 @@ public class Alumno extends Persona{
 
 	private Boolean validarClase() {
 	Boolean validar=false;
-	   if(this.tipoDeAbono == Abono.BRONCE && this.clasesInscriptas <= this.CANTIDAD_CLASE_BRONCE) {
+	   if(this.tipoDeAbono == Abono.BRONCE && this.clasesInscriptas < this.CANTIDAD_CLASE_BRONCE) {
 			validar=true;			
-		}else if(this.tipoDeAbono == Abono.PLATA && this.clasesInscriptas <= this.CANTIDAD_CLASE_PLATA){
+		}else if(this.tipoDeAbono == Abono.PLATA && this.clasesInscriptas < this.CANTIDAD_CLASE_PLATA){
 			validar=true;			
 		}else if(this.tipoDeAbono == Abono.ORO) {
 			validar=true;
 		}
 	   return validar;		
 	}
-
+	
+	/*
+	 * Se modifica el saldo que se le debe al gimnasio
+	 * en funcion del abono contratado
+	 */
 	public void facturarMes() {
 		Double saldo=0.0;
 		switch(this.tipoDeAbono) {
---
+++ test/ar/edu/unlam/pb2/gimnasio/ProfesorTest.java
@@ -0,0 +1,59 @@
+package ar.edu.unlam.pb2.gimnasio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProfesorTest {
+
+	@Test
+	public void queSePuedanCrearUnProfesorYSusAtributos() {
+		Profesor profesor = new Profesor("Martin", "Fernandez", 10000000, Genero.MASCULINO, TipoClase.FUNCIONAL);
+		
+		assertNotNull(profesor);
+	}
+	
+	@Test
+	public void queSePuedanAgregarEspecialidades() {
+		Profesor profesor = new Profesor("Martin", "Fernandez", 10000000, Genero.MASCULINO, TipoClase.FUNCIONAL);
+		Boolean valorEsperado = Boolean.TRUE;
+		Boolean valorObtenido;
+		
+		profesor.setEspecialidades(TipoClase.MUSCULACION);
+		valorObtenido = profesor.yaEsEspecialista(TipoClase.MUSCULACION);
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
+	
+	@Test
+	public void queSePuedanModificarElImporteADepositar() {
+		Profesor profesor = new Profesor("Martin", "Fernandez", 10000000, Genero.MASCULINO, TipoClase.FUNCIONAL);
+		Double valorEsperado = 1000.0;
+		Double valorObtenido;
+		
+		profesor.setImporteADepositar(1000.0);
+		
+		valorObtenido = profesor.getImporteADepositar();
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
+	
+	@Test
+	public void queSePuedanObtenerCorrectamenteLaCantidadDeEspecialidades() {
+		Profesor profesor = new Profesor("Martin", "Fernandez", 10000000, Genero.MASCULINO, TipoClase.FUNCIONAL);
+		Integer valorEsperado = 3;
+		Integer valorObtenido;
+		
+		profesor.setEspecialidades(TipoClase.MUSCULACION);
+		profesor.setEspecialidades(TipoClase.MUSCULACION);
+		profesor.setEspecialidades(TipoClase.MUSCULACION);
+		profesor.setEspecialidades(TipoClase.SALSA);
+		profesor.setEspecialidades(TipoClase.SALSA);
+		profesor.setEspecialidades(TipoClase.SALSA);
+		
+		valorObtenido = profesor.getCantidadEspecialidades();
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
+
+}
